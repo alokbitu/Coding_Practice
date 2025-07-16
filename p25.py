580930de04d05d969f982ff401c2fc6f11f23377
alok <alokp4004@gmailcom>
2025-07-16 15:26:55 +0530

    Pattern Coding

--- p25.py
+++ p25.py
@@ -5,3 +5,12 @@ O   O
  O O
 O    O
 '''
+
+n = 5
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i == j or j == n+1-i:
+            print("O",end=' ')
+        else:
+            print(" ",end=" ")
+    print()
--- p27.py
+++ p27.py
@@ -7,3 +7,10 @@
     1 1 1
       1
 '''
+
+n = 4
+for i  in range(n):
+    print('  '*(n-i-1)+"1 "*(2*i+1))
+
+for i in range(n-2,-1,-1):
+    print('  ' * (n - i - 1) + "1 " * (2 * i + 1))
